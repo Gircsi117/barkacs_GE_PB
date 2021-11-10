e939a772176cebdc9fee129f947d308bf787550a
Gircsi117 <gilianerik117@gmail.com>
2021-11-10 21:53:22 +0100

    Menu design

--- templates/menu.php
+++ templates/menu.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <ul class="navbar-nav nav-pills">
+    <ul class="navbar-nav nav-pills col">
      <li class="nav-item">
-        <a class="nav-link <?php active(1, $sorszam) ?>" href="index.php">Főoldal</a>
+        <a class="nav-link text-center <?php active(1, $sorszam) ?>" href="index.php">Főoldal</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php active(2, $sorszam) ?>" href="festekek.php">Festékek</a>
+        <a class="nav-link text-center <?php active(2, $sorszam) ?>" href="festekek.php">Festékek</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php active(3, $sorszam) ?>" href="faanyagok.php">Faanyagok</a>
+        <a class="nav-link text-center <?php active(3, $sorszam) ?>" href="faanyagok.php">Faanyagok</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php active(4, $sorszam) ?>" href="szerszamkatalogus.php">Szerszámkatalógus</a>
+        <a class="nav-link text-center <?php active(4, $sorszam) ?>" href="szerszamkatalogus.php">Szerszámkatalógus</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php active(5, $sorszam) ?>" href="rolunk.php">Rólunk</a>
+        <a class="nav-link text-center <?php active(5, $sorszam) ?>" href="rolunk.php">Rólunk</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link <?php active(6, $sorszam) ?>" href="blog.php">Blog</a>
+        <a class="nav-link text-center <?php active(6, $sorszam) ?>" href="blog.php">Blog</a>
       </li>
     </ul>
   </div>
