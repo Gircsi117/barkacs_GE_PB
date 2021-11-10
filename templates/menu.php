f1c26974169f0ef3fa3a7749b3b0b8e286ba2af2
Ramenslurper69 <petobalint09@gmail.com>
2021-11-10 12:13:51 +0100

    menü bootstrap + szerszamkatalogus.php létrehozás

--- templates/menu.php
+++ templates/menu.php
@@ -1,1 +1,24 @@
-aaa
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+     <li class="nav-item">
+        <a class="nav-link" href="index.php">Főoldal</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="festekek.php">Festékek</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="faanyagok.php">Faanyagok</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="szerszamkatalogus.php">Szerszámkatalógus</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="rolunk.php">Rólunk</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="blog.php">Blog</a>
+      </li>
+    </ul>
+  </div>
+</nav>
