491af3a52f729d2ee0b0044e52063cef8ae48bca
Gircsi117 <gilianerik117@gmail.com>
2021-11-12 22:56:38 +0100

    Faanyag +

--- faanyagok.php
+++ faanyagok.php
@@ -7,27 +7,31 @@
     $img = "rounded-circle col-7 col-md-10";
 ?>
 
-    <div class="container d-flex flex-column flex-md-row flex-wrap align-content-start">
-        <div class="<?php echo $imgdiv ?>">
-            <img src="img/bukk.jpg" alt="" class="<?php echo $img ?>">
-            <h3>Bükk</h3>
+    <div class="container">
+        <div class="d-flex flex-column flex-md-row flex-wrap align-content-start">
+            <div class="<?php echo $imgdiv ?>">
+                <img src="img/bukk.jpg" alt="" class="<?php echo $img ?>">
+                <a href=""><h3>Bükk</h3></a>
+            </div>
+            <div class="<?php echo $imgdiv ?>">
+                <img src="img/cseresznye.jpg" alt="" class="<?php echo $img ?>">
+                <a href=""><h3>Cseresznye</h3></a>
+            </div>
+            <div class="<?php echo $imgdiv ?>">
+                <img src="img/dio.jpg" alt="" class="<?php echo $img ?>">
+                <a href=""><h3>Dió</h3></a>
+            </div>
+            <div class="<?php echo $imgdiv ?>">
+                <img src="img/mahagoni.jpg" alt="" class="<?php echo $img ?>">
+                <a href=""><h3>Mahagóni</h3></a>
+            </div>
+            <div class="<?php echo $imgdiv ?>">
+                <img src="img/tolgy.jpg" alt="" class="<?php echo $img ?>">
+                <a href=""><h3>Tölgy</h3></a>
+            </div>
         </div>
-        <div class="<?php echo $imgdiv ?>">
-            <img src="img/cseresznye.jpg" alt="" class="<?php echo $img ?>">
-            <h3>Cseresznye</h3>
-        </div>
-        <div class="<?php echo $imgdiv ?>">
-            <img src="img/dio.jpg" alt="" class="<?php echo $img ?>">
-            <h3>Dió</h3>
-        </div>
-        <div class="<?php echo $imgdiv ?>">
-            <img src="img/mahagoni.jpg" alt="" class="<?php echo $img ?>">
-            <h3>Mahagóni</h3>
-        </div>
-        <div class="<?php echo $imgdiv ?>">
-            <img src="img/tolgy.jpg" alt="" class="<?php echo $img ?>">
-            <h3>Tölgy</h3>
-        </div>
+        <hr class="elvalaszto">
+        <h1>Szia</h1>
     </div>
 
 <?php include 'templates/footer.php' ?>
