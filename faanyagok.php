7c3a515f5ed80d7169e5b457315ac4ddbef34e09
Gircsi117 <gilianerik1107@gmail.com>
2021-11-12 09:35:51 +0100

    Faanyagok szekesztése

--- faanyagok.php
+++ faanyagok.php
@@ -2,8 +2,19 @@
 
 <?php include 'templates/menu.php' ?>
 
-<div class="container">
-    
+<div class="container d-flex flex-column flex-sm-row">
+    <div class="faanyag col-12 col-sm-3">
+        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
+        <h3></h3>
+    </div>
+    <div class="faanyag col-12 col-sm-3">
+        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
+        <h3></h3>
+    </div>
+    <div class="faanyag col-12 col-sm-3">
+        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
+        <h3></h3>
+    </div>
 </div>
 
 <?php include 'templates/footer.php' ?>
