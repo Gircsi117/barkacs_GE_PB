3bb164e8ff318188f92ff8b6ad90713f24b64e3c
Gircsi117 <gilianerik117@gmail.com>
2021-11-12 22:41:45 +0100

    Fügvény frissítés

--- templates/functions.php
+++ templates/functions.php
@@ -1,9 +1,8 @@
 <?php
 
     function active($feltetel, $oldalszam){
-        switch (($feltetel == $oldalszam)) {
-            case TRUE: echo "active"; break;
-            default: echo ""; break;
+        if ($feltetel == $oldalszam) {
+            echo "active";
         }
     }
 
