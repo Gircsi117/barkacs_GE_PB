55037e0de7ad2b4e1a6b918ddd41677ecd9dd349
Gircsi117 <gilianerik117@gmail.com>
2021-11-12 22:36:40 +0100

    Faanyag minták beillesztése + css

--- faanyagok.php
+++ faanyagok.php
@@ -2,19 +2,32 @@
 
 <?php include 'templates/menu.php' ?>
 
-<div class="container d-flex flex-column flex-sm-row">
-    <div class="faanyag col-12 col-sm-3">
-        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
-        <h3></h3>
+<?php
+    $imgdiv = "faanyag col-12 col-md-3 d-flex flex-column align-items-center mt-2";
+    $img = "rounded-circle col-7 col-md-10";
+?>
+
+    <div class="container d-flex flex-column flex-md-row flex-wrap align-content-start">
+        <div class="<?php echo $imgdiv ?>">
+            <img src="img/bukk.jpg" alt="" class="<?php echo $img ?>">
+            <h3>Bükk</h3>
+        </div>
+        <div class="<?php echo $imgdiv ?>">
+            <img src="img/cseresznye.jpg" alt="" class="<?php echo $img ?>">
+            <h3>Cseresznye</h3>
+        </div>
+        <div class="<?php echo $imgdiv ?>">
+            <img src="img/dio.jpg" alt="" class="<?php echo $img ?>">
+            <h3>Dió</h3>
+        </div>
+        <div class="<?php echo $imgdiv ?>">
+            <img src="img/mahagoni.jpg" alt="" class="<?php echo $img ?>">
+            <h3>Mahagóni</h3>
+        </div>
+        <div class="<?php echo $imgdiv ?>">
+            <img src="img/tolgy.jpg" alt="" class="<?php echo $img ?>">
+            <h3>Tölgy</h3>
+        </div>
     </div>
-    <div class="faanyag col-12 col-sm-3">
-        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
-        <h3></h3>
-    </div>
-    <div class="faanyag col-12 col-sm-3">
-        <img src="img/bukk.jpg" alt="" class="rounded-circle col-12">
-        <h3></h3>
-    </div>
-</div>
 
 <?php include 'templates/footer.php' ?>
