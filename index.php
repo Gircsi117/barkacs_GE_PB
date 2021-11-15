5de13376bf73e07e152d96fcf73bf9967efaa47c
Ramenslurper69 <petobalint09@gmail.com>
2021-11-15 11:09:15 +0100

    Főoldal váz fixed #3

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include 'templates/menu.php' ?>
 
 <div class="container">
+  <h1>Lorem Barkácsbolt</h1>
   <div id="carouselExampleCaptions" class="carousel carousel-dark slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -13,22 +14,22 @@
       <div class="carousel-item active">
         <img src="img/szerszamok.jpg" class="d-block w-75 m-auto" alt="...">
         <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Some representative placeholder content for the first slide.</p>
+          <h5>Minőségi szerszámok</h5>
+          <p></p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-75" alt="...">
+        <img src="img/faanyag.jpg" class="d-block w-75 m-auto" alt="...">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Some representative placeholder content for the second slide.</p>
+          <h5>Faanyag választék</h5>
+          <p></p>
         </div>
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-75" alt="...">
+        <img src="img/festek.jpg" class="d-block w-75 m-auto" alt="...">
         <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Some representative placeholder content for the third slide.</p>
+          <h5>Festékek minden árnyalatban</h5>
+          <p></p>
         </div>
       </div>
     </div>
@@ -41,6 +42,11 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
+  <h3 class="py-3">Üdvözöljük barkács üzletünk weboldalán</h3>
+  <p class="py-3">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nam ducimus veniam commodi neque tempore cum repudiandae tenetur aspernatur corporis dolore cumque in modi qui officiis, ipsa voluptate perferendis impedit.
+  Laudantium est sit, excepturi autem consectetur voluptatibus sunt veritatis qui laboriosam officiis quo corporis repudiandae veniam dolores deleniti molestias praesentium cumque enim omnis architecto possimus. Consectetur culpa libero nihil eum.
+  </p>
 </div>
 
 <?php include 'templates/footer.php' ?>
