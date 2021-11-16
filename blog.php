bbe550276556ab599c26b2080bf733667e126d7e
Ramenslurper69 <petobalint09@gmail.com>
2021-11-16 09:41:04 +0100

    blog váz fixed #7

--- blog.php
+++ blog.php
@@ -2,4 +2,17 @@
 
 <?php include 'templates/menu.php' ?>
 
+<div class="container text-start mt-3 mb-3">
+<p class="pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias iste voluptas soluta! Laudantium ipsam explicabo molestias, nihil recusandae modi ducimus velit vel possimus illo. Facilis debitis est magnam dolore vitae?
+Animi, est. Nesciunt corporis possimus ipsum eligendi velit earum excepturi eos aliquid, sunt laudantium ad ullam dolorem voluptate necessitatibus enim nobis molestias! Optio itaque reprehenderit dolorum dolorem natus aliquam blanditiis!
+Officia, hic eius vitae dolor maiores sequi iusto et libero nobis a rem dignissimos veritatis inventore sapiente corrupti ex perspiciatis earum amet, ut delectus ad fugit quaerat nam fugiat. Deleniti!
+Impedit ipsum error sed quaerat minima, enim, deserunt magni iusto temporibus quasi aliquam a sapiente cum accusamus, similique eligendi tempora eum. <br><br> Veritatis eius, minus aliquam qui distinctio et explicabo laudantium!
+Culpa doloremque magnam eveniet iure! Voluptas eum veniam explicabo fugiat architecto quibusdam cumque eveniet at consectetur. Saepe atque veritatis explicabo exercitationem facere. Atque magni harum qui, odio natus reprehenderit maiores.
+Eligendi repellat dicta, qui autem doloribus necessitatibus! Repudiandae cum, eius voluptatem soluta amet accusantium porro magnam debitis saepe odio commodi doloribus obcaecati minus illo iusto minima tenetur quis, vitae ea.
+Consectetur dolorem suscipit veritatis voluptatibus cumque ducimus deserunt quos accusantium, soluta sit in dignissimos voluptas aspernatur dolore? Aperiam, repellat enim non magnam laborum eum et, nulla assumenda voluptas ipsam at.
+Asperiores nostrum beatae expedita. Corrupti veniam fugiat esse voluptatibus. Cumque iusto ipsum quaerat nemo voluptatibus, numquam eius nesciunt possimus eveniet adipisci placeat dolorem, unde aut a cupiditate minus ratione nihil?
+Incidunt animi ipsum nam odio adipisci ullam quam fugit sint corrupti, quasi ex! Odit natus architecto amet dolores iusto dicta at omnis vel pariatur, minima maiores asperiores voluptatibus, eveniet totam.
+Velit alias facere assumenda eligendi enim numquam perferendis cupiditate sapiente sit repudiandae repellendus aperiam, amet placeat tenetur et distinctio iure sint aspernatur a ad quibusdam quam quas exercitationem! Fuga, odio.</p>
+</div>
+
 <?php include 'templates/footer.php' ?>
