e7912d547dca42941b99cf0ca12408035be4754e
Ramenslurper69 <petobalint09@gmail.com>
2021-11-19 09:35:38 +0100

    szerszámkatalógus gombok

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -10,16 +10,15 @@
 <div class="container mt-0 mt-sm-3 mb-0 mb-sm-3 p-3 d-flex flex-md-row flex-wrap justify-content-center">
     <div class="festek col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3">
         <img src="img/Szerszamok/colostok.jpg" alt="" class="col-12">
-        <h4 class="text-center">Széleskörő Colostok kínálat</h4>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim sunt obcaecati dolore aperiam animi sit quidem magnam! Voluptate nesciunt delectus doloremque corrupti officiis ex, tenetur ratione ea debitis culpa. Nostrum!</p>
-        
+        <h4 class="text-center">Széleskörű Colostok kínálat</h4>
+        <center><button type="button" class="btn btn-secondary btn-lg">Árajánlat igénylése</button></center>
     </div>
     <?php
         for ($i=1; $i < count($nevek); $i++) { 
             echo '<div class="festek col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3">',
                     '<img src="img/Szerszamok/',$elerheto[$i] ,'.jpg" alt="" class="col-12">',
-                    '<h4 class="text-center">Széleskörő ',$nevek[$i] ,' kínálat</h4>',
-                    '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim sunt obcaecati dolore aperiam animi sit quidem magnam! Voluptate nesciunt delectus doloremque corrupti officiis ex, tenetur ratione ea debitis culpa. Nostrum!</p>',
+                    '<h4 class="text-center">Széleskörű ',$nevek[$i] ,' kínálat</h4>',
+                    '<center><button type="button" class="btn btn-secondary btn-lg">Árajánlat igénylése</button></center>',
                 '</div>';
         }
     ?>
