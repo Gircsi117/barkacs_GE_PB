2e0356a45662efdfc7568ac739cf78c62e3cdac7
Gircsi117 <gilianerik1107@gmail.com>
2021-11-19 08:34:53 +0100

    Szerszámok kiegészítés

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -3,8 +3,8 @@
 <?php include 'templates/menu.php' ?>
 
 <?php
-    $nevek = ["Colostok", "Csavarhúzó", "Flex", "Fűrész", "Fúró", "Kalapács", "Láncfűrész", "Mérőszalag", "Metszőolló", "Pajszer"];
-    $elerheto = ["colostok", "csavarhuzo", "flex", "furesz", "furo", "kalapacs", "lancfuresz", "meroszalag", "ollo", "pajszer"];
+    $nevek = ["Colostok", "Csavarhúzó", "Flex", "Fűrész", "Fúró", "Kalapács", "Láncfűrész", "Mérőszalag", "Metszőolló", "Pajszer", "Talicska"];
+    $elerheto = ["colostok", "csavarhuzo", "flex", "furesz", "furo", "kalapacs", "lancfuresz", "meroszalag", "ollo", "pajszer", "talicska"];
 ?>
 
 <div class="container mt-0 mt-sm-3 mb-0 mb-sm-3 p-3 d-flex flex-md-row flex-wrap justify-content-center">
