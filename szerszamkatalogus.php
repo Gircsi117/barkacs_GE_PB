f865714bc102e5779036e05863b282f3684f7b13
Gircsi117 <gilianerik1107@gmail.com>
2021-11-19 10:21:31 +0100

    Szerszám gombok helyezése

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -8,17 +8,17 @@
 ?>
 
 <div class="container mt-0 mt-sm-3 mb-0 mb-sm-3 p-3 d-flex flex-md-row flex-wrap justify-content-center">
-    <div class="festek col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3">
+    <div class="szerszam col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3 d-flex flex-column">
         <img src="img/Szerszamok/colostok.jpg" alt="" class="col-12">
         <h4 class="text-center">Széleskörű Colostok kínálat</h4>
-        <center><button type="button" class="btn btn-secondary btn-lg">Árajánlat igénylése</button></center>
+        <button type="button" class="btn btn-secondary btn-lg align-self-end col-12">Árajánlat igénylése</button>
     </div>
     <?php
         for ($i=1; $i < count($nevek); $i++) { 
-            echo '<div class="festek col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3">',
+            echo '<div class="szerszam col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3 d-felx flex-column align-items-start">',
                     '<img src="img/Szerszamok/',$elerheto[$i] ,'.jpg" alt="" class="col-12">',
                     '<h4 class="text-center">Széleskörű ',$nevek[$i] ,' kínálat</h4>',
-                    '<center><button type="button" class="btn btn-secondary btn-lg">Árajánlat igénylése</button></center>',
+                    '<button type="button" class="btn btn-secondary btn-lg col-12 d-block mt-150">Árajánlat igénylése</button>',
                 '</div>';
         }
     ?>
