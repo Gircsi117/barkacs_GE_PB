1b5f6c04abcd6c3bc0701eccbf0a488352742776
Ramenslurper69 <petobalint09@gmail.com>
2021-11-19 09:09:15 +0100

    blog tartalom

--- blog.php
+++ blog.php
@@ -2,17 +2,48 @@
 
 <?php include 'templates/menu.php' ?>
 
-<div class="container text-start mt-0 mt-sm-3 mb-0 mb-sm-3">
-<p class="pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias iste voluptas soluta! Laudantium ipsam explicabo molestias, nihil recusandae modi ducimus velit vel possimus illo. Facilis debitis est magnam dolore vitae?
-Animi, est. Nesciunt corporis possimus ipsum eligendi velit earum excepturi eos aliquid, sunt laudantium ad ullam dolorem voluptate necessitatibus enim nobis molestias! Optio itaque reprehenderit dolorum dolorem natus aliquam blanditiis!
-Officia, hic eius vitae dolor maiores sequi iusto et libero nobis a rem dignissimos veritatis inventore sapiente corrupti ex perspiciatis earum amet, ut delectus ad fugit quaerat nam fugiat. Deleniti!
-Impedit ipsum error sed quaerat minima, enim, deserunt magni iusto temporibus quasi aliquam a sapiente cum accusamus, similique eligendi tempora eum. <br><br> Veritatis eius, minus aliquam qui distinctio et explicabo laudantium!
-Culpa doloremque magnam eveniet iure! Voluptas eum veniam explicabo fugiat architecto quibusdam cumque eveniet at consectetur. Saepe atque veritatis explicabo exercitationem facere. Atque magni harum qui, odio natus reprehenderit maiores.
-Eligendi repellat dicta, qui autem doloribus necessitatibus! Repudiandae cum, eius voluptatem soluta amet accusantium porro magnam debitis saepe odio commodi doloribus obcaecati minus illo iusto minima tenetur quis, vitae ea.
-Consectetur dolorem suscipit veritatis voluptatibus cumque ducimus deserunt quos accusantium, soluta sit in dignissimos voluptas aspernatur dolore? Aperiam, repellat enim non magnam laborum eum et, nulla assumenda voluptas ipsam at.
-Asperiores nostrum beatae expedita. Corrupti veniam fugiat esse voluptatibus. Cumque iusto ipsum quaerat nemo voluptatibus, numquam eius nesciunt possimus eveniet adipisci placeat dolorem, unde aut a cupiditate minus ratione nihil?
-Incidunt animi ipsum nam odio adipisci ullam quam fugit sint corrupti, quasi ex! Odit natus architecto amet dolores iusto dicta at omnis vel pariatur, minima maiores asperiores voluptatibus, eveniet totam.
-Velit alias facere assumenda eligendi enim numquam perferendis cupiditate sapiente sit repudiandae repellendus aperiam, amet placeat tenetur et distinctio iure sint aspernatur a ad quibusdam quam quas exercitationem! Fuga, odio.</p>
+<div class="container mt-0 mt-sm-3 mb-0 mb-sm-3">
+    <h1>Rakj Rendet a Műhelyedben</h1>
+    <div class="text-start p-5">
+        <p">Egy rendezett műhely nem csak a szemnek öröm, de a munka is lényegesen hatékonyabb, gyorsabb, ha minden szerszámunk, és gépünk kéznél van. érdemes félévente, vagy akár gyakrabban átnézni,      rendszerezni,    beszerezni a praktikusabbnál praktikusabb rendszerezőket, tárolókat</p>
+        <br><br>
+        <p>Ne halogasd tovább, rakj rendet a műhelyedben! A műhelyben vagy a garázsban a szerszámok közötti rendrakást az ember nem végzi minden nap, sőt talán minden évben sem. Pedig gyakrabban kellene, mint szoktuk. Miért? Mert a rendrakással mindennek praktikusan helye lesz, a gépeket, szerszámokat nem kell folyton keresni. A műhely rendbetételével egyidejűleg lehetőség nyílik átnézni a meglévő eszközöket, barkácsgépeket, fogyóeszközöket, pótolni, javítani, amit szükséges. Ésszerű rendszerezéssel több hely marad a munkához, nem lesznek útban a dolgok és átláthatóbb lesz az egész műhely.</p>
+        <br><br>
+        <h4>Rakj rendet a műhelyedben – hogyan fogj neki?</h4>
+        <br>
+        <p>Először is gondold át, milyen szerszámaid vannak, milyen gépeket tartasz a műhelyben és milyen tárolók állnak rendelkezésre. Rendet rakni akkor lehet, ha néhány alapvető rendszerező megvan hozzá, mint például fali akasztók, fali mágneses tárolók, dobozok, szerszámkocsik, polcok, polcrendszerek. Fontos, hogy legyen a műhelyben egy munkaasztal, mert a rendszerezést legpraktikusabban innen indíthatjuk.
+        Szánjuk rá az időt, például egy laza hétvégi napot, amikor nem kell kapkodni, sietni, és átgondoltan tudunk pakolni. A rendrakás a műhelyben több órát fog igénybe venni, de megéri a fáradság, hiszen cserébe kapunk egy jól átlátható, rendszerezett helyiséget, ahol élmény lesz a munka. „Rend a lelke mindennek” – tartja a mondás, és ez nem kizárólag a rendrakásra vonatkozik, a rendszerezés is fontos része a folyamatnak. Logikus, következetes módon. Ha pedig rend van körülöttünk, a munkavégzés is sokkal inkább összeszedett, nyugodtabb és gyorsabb lesz.</p>
+        <br>
+        <img src="img/blog/rakj-rendet-a-muhelyben.jpg" class="img-fluid rounded mx-auto d-block">
+        <br><br>
+        <h4>Rakj rendet a műhelyedben – a munkaasztal a kiindulópont</h4>
+        <br>
+        <p>A munkaasztal valószínűleg az egyik olyan biztos pont a műhelyben, ahol a legtöbb időt töltjük, ha valamit csinálni kell, és nem a helyszínen végezzük.  A munkaasztalon lehetőleg semmi más ne fogja a helyet, csak a satupad, esetleg, ha van asztali fúrógépünk, akkor az, de minden mást pakoljunk el innen, hiszen ez egy munkaasztal, munkához való és nem szerszámtárolásra. Nézzük meg az asztalt, hogy nem -e billeg, stabilan áll, kellően magas -e és töröljük át a felületét, legyen tiszta.</p>
+        <br><br>
+        <h4>Fali szerszámtárolás, rendszerezés</h4>
+        <br>
+        <p>Ha lehetőség van rá, a munkaasztal köré a falra szereljünk fel néhány akasztós és mágneses fali tárolót, sőt dobozok tárolására alkalmas panelt is, mely igazán sokoldalúan használható lesz a későbbiekben. Kaphatóak már olyan multifunkciós fali rendszerezők, melyeken tárolódobozok számára is van hely, de akasztók, kampók, polcok is lehetőséget adnak rajtuk mindenféle eszköz megfelelő elhelyezésére. Egy-két ilyen fali rendszerező hosszútávon megtérül, semmit nem kell kétszer megvenni, mert nem találjuk, vagy azt gondoljuk nincs, esetleg elveszett. Minden kéznél lesz fix helyen amikor szükséges! Így rakj rendet a műhelyedben!
+        A fali rendszerezőkre, mágneses tartókra vagy akasztókra érdemes a kéziszerszámokat tenni, mint például különböző kulcsok, csavarhúzók, fogók, fúrószárak mégpedig méret szerint elhelyezve. A fali rendszerezőn található kisebb tárolódobozok tökéletesen alkalmasak különböző szegek, csavarok, tiplik, alátétek, anyák, vagy bármilyen apró eszközök, mint például csipeszek szortírozására, de lehetnek bennük drótok, madzagok, sőt sniccer, ragasztószalag, olló vagy például mérőszalag.</p>
+        <br>
+        <img src="img/blog/muhely-rendszerezes.jpg" class="img-fluid rounded mx-auto d-block">
+        <br><br>
+        <h4>Rakj rendet a műhelyedben és folytasd a polcokkal!</h4>
+        <br>
+        <p>A polcos tárolókon érdemes elhelyezni a szerszámgépeket, a fúrókat, csiszolókat, csavarozókat, akkumulátorokat, fűrészeket, hőlégfúvót, tűzőgépet vagy tűzőgépet, ragasztópisztolyt, egyszóval mindenféle barkácsgépet. Érdemes magasabbra tenni azokat, amelyekre ritkán van csak szükség, és alsóbb polcokra, szemmagasságba azokat, amelyek akár napi szinten, de legalábbis gyakran kellenek. Ha időnk engedi, a gépeket töröljük át, nézzük meg nem-e sérültek, vezetékeik rendben vannak, késeik kellően élesek. <br><br>
+        Egy polc soron érdemes nagyobb tárolódobozokat elhelyezni, és ebbe téma szerint beletenni eszközöket, szerszámokat. Például az egyikbe mehet biciklipumpa, gumibelső stb., másikba kinyomópisztoly, tiszta ecsetek, festő hengerek. Lehet egy doboz a csiszolópapíroknak, csiszolótuskóknak, keféknek, drótkeféknek, egy a kőművesszerszámoknak, mint a simító, glettvas, kőműveskanál. Másik tárolóba mehetnek a nagyobb kéziszerszámok, melyek nem valók a fali akasztókra, mondjuk a különböző kalapácsok, feszítővasak, egyéb nagyobb vagy nehezebb kéziszerszámok. Annyi dobozunk legyen a polcon, hogy minden kéziszerszámot megfelelően rendszerezve tudjunk elpakolni.
+        A falra érdemes néhány nagyobb akasztót, kampót befúrni, és azokra fel lehet tenni például az alu létrát, vagy más nagyobb terjedelmű, de felakasztható eszközt, a plafonra fúrva egy biciklitárolót pedig a kerékpár helye is megoldott.</p>
+        <br>
+        <img src="img/blog/fali-szerszamtarolas.jpg" class="img-fluid rounded mx-auto d-block">
+        <br><br>
+        <h4>Ha kész vannak a polcok, rakj rendet a műhelyedben és pakold el a nagyobb gépeket, kerti eszközöket!</h4>
+        <br>
+        <p>A nagyobb gépeket, mint a magasnyomású mosó, fűnyíró, gyepszellőztető, rotációs kapa érdemes a műhelyben egy sarokba beállítani, ahol nincsenek útban, de könnyen elővehetők amikor szükséges. Ha sok kerti gépünk, szerszámunk van, állítsunk be nekik egy külön polcrendszert, melynek az oldalára tehetünk különböző nyélméretek tárolására alkalmas befogófejeket. Ezekbe fogathatjuk a seprűnyelet, a gereblyét, ásót, kapát, lombseprűt, lapátot. A polcokra helyezzük el a kerti gépeket, amik nem túl nagyok, például lombszívó, fűkasza, ágvágó, permetező stb. Itt is tároljuk dobozokban az apróbb dolgokat, ami a kertben vagy a kerti munkához kellhet, metszőollót, ültetőkanalat, kiskapát, virágollót.</p>
+        <br><br>
+        <h4>A műhely rendszerezés előnyei</h4>
+        <br>
+        <p>A műhely rendbetételére megéri időt szánni. Előkerülhetnek rég nem látott eszközök, gépek is, ugyanakkor azt is látni fogjuk, mi az, ami hiányzik, be kell szerezni, csavarosdobozokat, alátétes, anyás dobozokat feltölteni, csiszolópapírokat pótolni, mindent, ami fogyóeszköz. <br><br>
+        Egy rendben tartott műhelyben öröm lesz a munka, hiszen minden kéznél van, minden megvan, nem kell keresni semmit. Több hely marad a feladatok elvégzésére és több idő a pihenésre. Szánjuk rá az időt és az energiát, rakjunk rendet a műhelyben, aztán jutalmazzuk meg magunkat egy sörrel! Vagy egy új géppel! Esetleg mindkettővel. Ha pedig elfáradtunk a műhely, vagy garázs rendbetétele közben, pihenés képpen érdemes olvasgatni a többi bejegyzésünket is, például arról, hogyan kell házilag kerti grillt építeni.</p>
+    </div>
 </div>
 
 <?php include 'templates/footer.php' ?>
