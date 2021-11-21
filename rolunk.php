9087207d49b9d639c955133a7b37fdc3cc37fc9d
Ramenslurper69 <76950363+Ramenslurper69@users.noreply.github.com>
2021-11-21 17:51:56 +0100

    Lorem helyettesítése tartalommal fixed #16

--- rolunk.php
+++ rolunk.php
@@ -5,15 +5,24 @@
 <div class="container mt-0 mt-sm-3 mb-0 mb-sm-3 p-3">
     <h1>Rólunk</h1>
     <div class="text-start">
-        <p class="pt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus atque impedit repudiandae blanditiis magni autem, eum itaque modi necessitatibus, quam ducimus pariatur nulla perspiciatis neque porro amet sapiente accusantium eaque?</p>
+        <p class="pt-3">
+        A Lorem barkácsbolt 2021-ben kétszemélyes egyéni vállalkozásként alapvetően faanagyok forgalmazásával indult. Azóta önállóan működő üzletté nőtte ki magát. A baráti légkör szerves része lett arculatunknak, de termékeink száma az évek során többszörösére bővült.
+        Jelenleg a faanyagok forgalmazása mellett szerszámok, festékek, elektromos kisgépek és barkács termékek forgalmazásával foglalkozunk.
+        </p>
         <hr id="vonal">
-        <div class="d-flex flex-column bd-highlight mb-3">
+        <div class="d-flex flex-column bd-highlight mb-3 mx-5">
             <div class="d-flex p-2 bd-highlight flex-column flex-md-row">
                 <img src="img/erik.png" class="img-fluid col-12 col-md-3" alt="Gilián Erik">
-                <p class="align-self-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus saepe molestiae, facilis facere labore ullam illum magni debitis. Debitis beatae animi similique ex! Minima veritatis quos tempore. Aliquam, laudantium deserunt!</p>
+                <div class="align-self-center">
+                    <h1>Erik</h1>
+                    <p>Fakéreg szakértő - Idegennyelvi gyors és géplakatos - Colostok kiegyenesítő - Vasárnap hajnali flexelés rajongó - Farágó - Türelmesen Bálintra várakozó - Falevél festő - CEO</p>
+                </div>
             </div>
             <div class="d-flex p-2 bd-highlight flex-column-reverse flex-md-row">            
-                <p class="align-self-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus saepe molestiae, facilis facere labore ullam illum magni debitis. Debitis beatae animi similique ex! Minima veritatis quos tempore. Aliquam, laudantium deserunt!</p>
+                <div class="align-self-center">
+                    <h1>Bálint</h1>
+                    <p>Diplomás évgyűrű számoló - Ujjatlan asztalos - Manuális esztergályos - Volt faszállító - Gyors és gépjármüvezető - Pajszer professzor - Kommunikációs Almanager aszisztens helyettes</p>
+                </div>
                 <img src="img/balint.png" class="img-fluid col-12 col-md-3" alt="Pető Bálint">
             </div>
         </div>
