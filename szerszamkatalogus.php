c37fcc92911b8683fcc1680df5849df83f9eea44
Gircsi117 <gilianerik1107@gmail.com>
2021-11-22 12:48:27 +0100

    Szerszámok minta

--- szerszamkatalogus.php
+++ szerszamkatalogus.php
@@ -8,18 +8,10 @@
 ?>
 
 <div class="container mt-0 mt-sm-3 mb-0 mb-sm-3 p-3 d-flex flex-md-row flex-wrap justify-content-center">
-    <div class="szerszam col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3 d-flex flex-column">
-        <img src="img/Szerszamok/colostok.jpg" alt="" class="col-12">
-        <h4 class="text-center">Széleskörű Colostok kínálat</h4>
-        <button type="button" class="btn btn-secondary btn-lg align-self-end col-12"><a href="arajanlat.php">Árajánlat igénylése</a></button>
-    </div>
+    
     <?php
-        for ($i=1; $i < count($nevek); $i++) { 
-            echo '<div class="szerszam col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3 d-felx flex-column align-items-start">',
-                    '<img src="img/Szerszamok/',$elerheto[$i] ,'.jpg" alt="" class="col-12">',
-                    '<h4 class="text-center">Széleskörű ',$nevek[$i] ,' kínálat</h4>',
-                    '<button type="button" class="btn btn-secondary btn-lg col-12 d-block mt-150"><a href="arajanlat.php">Árajánlat igénylése</a></button>',
-                '</div>';
+        for ($i=0; $i < count($nevek); $i++) { 
+            include 'templates/szerszam_minta.php';
         }
     ?>
 </div>
---
+++ templates/szerszam_minta.php
@@ -0,0 +1,5 @@
+<div class="szerszam col-lg-3 col-md-4 col-sm-6 col-12 border border-1 border-dark text-start p-3 d-flex flex-column">
+    <img src="img/Szerszamok/<?php echo $elerheto[$i]?>.jpg" alt="" class="col-12">
+    <h4 class="text-center">Széleskörű <?php echo $nevek[$i]?> kínálat</h4>
+    <button type="button" class="btn btn-secondary btn-lg align-self-end col-12"><a href="arajanlat.php">Árajánlat igénylése</a></button>
+</div>
