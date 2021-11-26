0dc139c4cefb9945ec89bb3beff5510c1431c515
Gircsi117 <gilianerik1107@gmail.com>
2021-11-26 08:43:39 +0100

    fixed #23

--- rolunk.php
+++ rolunk.php
@@ -10,17 +10,17 @@
         Jelenleg a faanyagok forgalmazása mellett szerszámok, festékek, elektromos kisgépek és barkács termékek forgalmazásával foglalkozunk.
         </p>
         <hr id="vonal">
-        <div class="d-flex flex-column bd-highlight mb-3 mx-5">
+        <div class="rolunk d-flex flex-column bd-highlight mb-3 mx-5">
             <div class="d-flex p-2 bd-highlight flex-column flex-md-row">
                 <img src="img/erik.png" class="img-fluid col-12 col-md-3" alt="Gilián Erik">
                 <div class="align-self-center">
-                    <h1>Erik</h1>
+                    <h1 class="text-center text-md-start bg-dark">Erik</h1>
                     <p>Fakéreg szakértő - Idegennyelvi gyors és géplakatos - Colostok kiegyenesítő - Vasárnap hajnali flexelés rajongó - Farágó - Türelmesen Bálintra várakozó - Falevél festő - CEO</p>
                 </div>
             </div>
             <div class="d-flex p-2 bd-highlight flex-column-reverse flex-md-row">            
                 <div class="align-self-center">
-                    <h1>Bálint</h1>
+                    <h1 class="text-center text-md-start bg-dark">Bálint</h1>
                     <p>Diplomás évgyűrű számoló - Ujjatlan asztalos - Manuális esztergályos - Volt faszállító - Gyors és gépjármüvezető - Pajszer professzor - Kommunikációs Almanager aszisztens helyettes</p>
                 </div>
                 <img src="img/balint.png" class="img-fluid col-12 col-md-3" alt="Pető Bálint">
