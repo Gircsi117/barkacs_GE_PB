ee91b5d3a15d0e6fbba9f65973c10e544874a2d8
Gircsi117 <gilianerik117@gmail.com>
2022-03-25 22:52:44 +0100

    Comment kivétel

--- templates/festek_minta.php
+++ templates/festek_minta.php
@@ -1,6 +1,5 @@
 <div class="festek col-lg-3 col-md-4 col-sm-6 col-12 p-3 border border-1 border-dark text-start">
     <img src="img/vodor.png" alt="" class="col-12" style="">
-    <!--<img src="img/vodor.png" alt="" class="col-12 vodor_kep" style="<?php echo $filter[$i]?> opacity(40%);">-->
     <div style="background-color:<?php echo $szinkod[$i] ?>; width:70%; height:50px; margin:10px auto; border:1px solid black; border-radius:15px"></div>
     <h4 class="m-0 text-center">Albert beltéri falfesték <?php echo $szinnev[$i]?> (2 liter)</h4>
     <p class="m-0"><span class="fw-bold">Gyártó: </span>Hat szivárvány nyrt.</p>
